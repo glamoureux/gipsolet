c7d6089e307aecadbf79067641a9ffc09c11435c
Marmau <marc.maury@gmail.com>
2012-12-21 11:48:37 +0100

    Test

--- src/lamaro/gipsolet/MainActivity.java
+++ src/lamaro/gipsolet/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		// Creation
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
