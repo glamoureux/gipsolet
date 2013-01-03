9328815206190b37ced7db0ba1dc22a2dcaa4096
Marc <marc.maury@gmail.com>
2013-01-03 13:40:21 +0100

    test domicile

--- src/lamaro/gipsolet/MainActivity.java
+++ src/lamaro/gipsolet/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// Creation
+		// Creation test
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
