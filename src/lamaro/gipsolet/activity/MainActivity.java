6a262968c721494a48634a7098891ae49124e166
Marc <marc.maury@gmail.com>
2013-01-14 14:10:54 +0100

    Suite interface recherche

--- src/lamaro/gipsolet/activity/MainActivity.java
+++ src/lamaro/gipsolet/activity/MainActivity.java
@@ -17,6 +17,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.graphics.PointF;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity implements RoutingResultHandler {
 
@@ -27,23 +28,27 @@ public class MainActivity extends Activity implements RoutingResultHandler {
 		setContentView(R.layout.activity_main);
 
 		// Tests !
-		Database db = new Database(this);
-		db.open();
-
-		List<Building> buildings = db.getBuildings();
-
-		PointF p = new PointF(43.634654f, 3.862047f);
-		for (Building b : buildings) {
-			System.out.println(b.id + " " + b.isInBuilding(p));
-		}
-
-		RoutingTask r = new RoutingTask(this);
-		r.execute(new PointF(43.614799f, 3.886697f), new PointF(43.631983f, 3.861178f));
-		
-		for (Object o: db.search("secre info")) {
-			System.out.println(o);
-		}
-		db.close();
+//		Database db = new Database(this);
+//		db.open();
+//
+//		List<Building> buildings = db.getBuildings();
+//
+//		PointF p = new PointF(43.634654f, 3.862047f);
+//		for (Building b : buildings) {
+//			System.out.println(b.id + " " + b.isInBuilding(p));
+//		}
+//
+//		RoutingTask r = new RoutingTask(this);
+//		r.execute(new PointF(43.614799f, 3.886697f), new PointF(43.631983f, 3.861178f));
+//		
+//		for (Object o: db.search("secre info")) {
+//			System.out.println(o);
+//		}
+//		db.close();
+	}
+	
+	public void onClickSearchEditText(View v) {
+		onSearchRequested();
 	}
 
 	@Override
--- src/lamaro/gipsolet/activity/SearchableActivity.java
+++ src/lamaro/gipsolet/activity/SearchableActivity.java
@@ -16,33 +16,51 @@ public class SearchableActivity extends ListActivity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    
-	    // Get the intent, verify the action and get the query
-	    Intent intent = getIntent();
-	    if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-	      String query = intent.getStringExtra(SearchManager.QUERY);
-	      doMySearch(query);
-	    }
-	    
-	    setListAdapter(new CampusEntitiesAdapter(this, R.layout.campus_entity_search_item));
+		super.onCreate(savedInstanceState);
+		System.out.println("Je suis ici");
+
+
+		setListAdapter(new CampusEntitiesAdapter(this, R.layout.campus_entity_search_item));
+
+		System.out.println("ICI = " + super.getListAdapter());
+		System.out.println("ICI MDR = " + getListAdapter());
+		
+		handleIntent(getIntent());
+
+		setDefaultKeyMode(DEFAULT_KEYS_SEARCH_LOCAL);
+	}
+
+	@Override
+	protected void onNewIntent(Intent intent) {
+		System.out.println("je suis la");
+		setIntent(intent);
+		handleIntent(intent);
 	}
 
 	private void doMySearch(String query) {
+		System.out.println("REQUETE = " + query);
 		Database db = new Database(this);
 		db.open();
 
 		List<CampusEntity> result = db.search(query);
+		System.out.println(getListAdapter());
 		getListAdapter().set(result);
-		
+
 		db.close();
 	}
-	
+
+	private void handleIntent(Intent intent) {
+		if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+			String query = intent.getStringExtra(SearchManager.QUERY);
+			doMySearch(query);
+		}
+	}
+
 	@Override
 	public CampusEntitiesAdapter getListAdapter() {
 		return (CampusEntitiesAdapter) super.getListAdapter();
 	}
-	
+
 	@Override
 	public void setListAdapter(ListAdapter adapter) {
 		super.setListAdapter((CampusEntitiesAdapter) adapter);
