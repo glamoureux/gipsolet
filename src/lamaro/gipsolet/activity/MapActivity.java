a321ab4183d23a8ab529fc66d3ea6eeed050f765
Marc <marc.maury@gmail.com>
2013-01-26 11:22:58 +0100

    Message indiquant que les maps bugue sur les avd pour éviter un plantage massif

--- src/lamaro/gipsolet/activity/MapActivity.java
+++ src/lamaro/gipsolet/activity/MapActivity.java
@@ -20,52 +20,46 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.Toast;
 
 public class MapActivity extends FragmentActivity {
-	
-	private GoogleMap map;
-	
-	PolygonOptions campus = new PolygonOptions()
-		.add(
-			new LatLng(43.629614f, 3.867531f),
-			new LatLng(43.62963f, 3.869998f),
-			new LatLng(43.634755f, 3.86811f),
-			new LatLng(43.634056f, 3.863583f),
-			new LatLng(43.635578f, 3.861523f),
-			new LatLng(43.635066f, 3.859849f),
-			new LatLng(43.631043f, 3.860621f))
-		.fillColor(Color.argb(128, 255, 0, 0))
-		.strokeWidth(1.0f);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-    	super.onCreate(savedInstanceState);
-        setContentView(R.layout.map);
-        handleIntent();
-        
-        System.out.println(ConnectionResult.SUCCESS);
-        System.out.println(ConnectionResult.SERVICE_MISSING);
-        System.out.println(ConnectionResult.SERVICE_VERSION_UPDATE_REQUIRED);
-        System.out.println(ConnectionResult.SERVICE_DISABLED);
-        System.out.println(ConnectionResult.SERVICE_INVALID);
-        System.out.println("=> " + GooglePlayServicesUtil.isGooglePlayServicesAvailable(this));
-        
-        map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
-        map.addPolygon(campus);
-        map.setOnCameraChangeListener(new OnCameraChangeListener() {
-            @Override
-            public void onCameraChange(CameraPosition arg0) {
-            	Builder builder = new LatLngBounds.Builder();
-        		for(LatLng latlng : campus.getPoints()) {
-        			builder.include(latlng);
-        		}
-            	map.animateCamera(CameraUpdateFactory.newLatLngBounds(builder.build(), 30));
-                map.setOnCameraChangeListener(null);
-            }
-        });
-    }
-    
-    private void handleIntent() {
+	private GoogleMap map;
+
+	PolygonOptions campus = new PolygonOptions()
+			.add(new LatLng(43.629614f, 3.867531f), new LatLng(43.62963f, 3.869998f), new LatLng(43.634755f, 3.86811f),
+					new LatLng(43.634056f, 3.863583f), new LatLng(43.635578f, 3.861523f),
+					new LatLng(43.635066f, 3.859849f), new LatLng(43.631043f, 3.860621f))
+			.fillColor(Color.argb(128, 255, 0, 0)).strokeWidth(1.0f);
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.map);
+		handleIntent();
+
+		System.out.println(ConnectionResult.SUCCESS == GooglePlayServicesUtil.isGooglePlayServicesAvailable(this));
+		if (ConnectionResult.SUCCESS == GooglePlayServicesUtil.isGooglePlayServicesAvailable(this)) {
+			map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+			map.addPolygon(campus);
+			map.setOnCameraChangeListener(new OnCameraChangeListener() {
+				@Override
+				public void onCameraChange(CameraPosition arg0) {
+					Builder builder = new LatLngBounds.Builder();
+					for (LatLng latlng : campus.getPoints()) {
+						builder.include(latlng);
+					}
+					map.animateCamera(CameraUpdateFactory.newLatLngBounds(builder.build(), 30));
+					map.setOnCameraChangeListener(null);
+				}
+			});
+		} else {
+			Toast.makeText(this, R.string.map_bug_on_avd, Toast.LENGTH_SHORT).show();
+			finish();
+		}
+	}
+
+	private void handleIntent() {
 		Intent intent = getIntent();
-    }
+	}
 }
